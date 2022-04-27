96bf1feace32ceadc7a2e8f31bc8430e7af38fb5
takuto_honda <krt.tc.se.1997@gmail.com>
2022-04-27 09:25:18 +0900

    priceのvalidatesを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,4 +24,5 @@ class Item < ApplicationRecord
     validates :shipping_day_id
   end
 
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
