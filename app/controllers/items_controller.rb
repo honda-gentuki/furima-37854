e7c48cbf9949477dd5b432f105f7dae56f1a2df0
takuto_honda <krt.tc.se.1997@gmail.com>
2022-04-28 20:10:00 +0900

    before_actionとprivateの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,15 +20,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
+  private
+
   def item_params
     params.require(:item).permit(
       :image, :name, :description, :condition_id, :category_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
