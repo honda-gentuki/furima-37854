3620e3297c6c2ba8caf279ea6b7948fea7668f51
takuto_honda <krt.tc.se.1997@gmail.com>
2022-04-29 20:36:02 +0900

    購入画面の表示の訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @item = Item.find(params[:item_id])
+    @shipping_address = ShippingAddress.new
   end
 
-  
+  def create
+  end
+
 end
