5d88a7bb9ea3e754549ba45ad355c9b78b7ed93c
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-01 14:35:18 +0900

    余分なprefecture_idの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,7 +23,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :shipping_area_id).merge(
+    params.require(:order_address).permit(:postal_code, :city, :address, :building, :phone_number, :shipping_area_id).merge(
       user_id: current_user.id, item_id: params[:item_id]
     )
   end
