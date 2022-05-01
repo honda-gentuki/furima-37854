7bf4e839485e2fb9942b7b35968f8a121abffc55
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-01 14:28:35 +0900

    order_addressにitemとuserの情報追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    @item = FactoryBot.create(:item)
+    @user = FactoryBot.create(:user)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+  end
+
+  describe '商品購入' do
+    context '商品が購入できる場合' do
+      it '全ての項目が正しく入力されていれば出品できる' do
+        expect(@order_address).to be_valid
+      end
+    end
   end
 end
