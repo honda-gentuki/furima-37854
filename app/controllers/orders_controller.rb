fb0c8eefe9fd6049cdc3ce9685ddbba18d9a8b2b
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-01 19:29:56 +0900

    出品者本人を商品購入画面から弾く

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_item, only: [:index, :create]
+  before_action :current_item_user, :sold_out
 
   def index
     @order_address = OrderAddress.new
@@ -37,4 +38,9 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def current_item_user
+    redirect_to root_path if current_user.id == @item.user.id
+  end
+
 end
