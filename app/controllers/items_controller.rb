997a36d3d90db8a0d673c3cd7ee3a7e545de29a7
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-02 15:58:01 +0900

    URLに直接入力されても編集ページへ飛ばないように追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,7 +55,7 @@ class ItemsController < ApplicationController
   end
 
   def not_current_user
-    if @item.user_id != current_user.id
+    if @item.user_id != current_user.id || @item.order.present?
       redirect_to root_path
     end
   end
